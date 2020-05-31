6dfdf1bec08b36c41302f69b1209f33306f8a299
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-05-31 15:38:57 -0500

    adds comparison

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,14 @@
 // this is an alias to use the io module
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
     println!("Please input a guess:");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
 
     // definition of the guess variable as mutable
     // the :: syntax means an asociated function to a type
@@ -16,5 +21,13 @@ fn main() {
         // in case the Result is an error, the expect method will crash the progam
         .expect("Failed to read line");
 
+    // we parse the guess to u32
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
