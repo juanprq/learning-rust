8f8dfbb0ba99c7abd3a6e53115a043ce442e5406
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-05-30 23:00:04 -0500

    adds some comments

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,11 +6,14 @@ fn main() {
     println!("Please input a guess:");
 
     // definition of the guess variable as mutable
+    // the :: syntax means an asociated function to a type
     let mut guess = String::new();
     // passing a mutable reference to guess, we need to pass a reference
     // because guess is not a primitive
     io::stdin()
         .read_line(&mut guess)
+        // the way that expect works, is that read_line returns a Result type
+        // in case the Result is an error, the expect method will crash the progam
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
