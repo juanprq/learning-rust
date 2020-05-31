7425d21af6db9be57a435d456335286a0b3e048f
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-05-30 22:50:30 -0500

    adds cargo fmt execution

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,8 +5,13 @@ fn main() {
     println!("Guess the number!");
     println!("Please input a guess:");
 
+    // definition of the guess variable as mutable
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    // passing a mutable reference to guess, we need to pass a reference
+    // because guess is not a primitive
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
 }
